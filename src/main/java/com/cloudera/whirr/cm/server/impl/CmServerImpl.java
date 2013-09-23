c438ac6257fba410647368d3e9c3b1cc61208164
ggear <graham.gear@gmail.com>
2013-09-23 17:11:18 +0100

    Add Solr init

--- src/main/java/com/cloudera/whirr/cm/server/impl/CmServerImpl.java
+++ src/main/java/com/cloudera/whirr/cm/server/impl/CmServerImpl.java
@@ -1138,6 +1138,8 @@ public class CmServerImpl implements CmServer {
     case SOLR:
       if (versionApi >= 4) {
         execute(apiResourceRootV4.getClustersResource().getServicesResource(getName(cluster))
+            .initSolrCommand(cluster.getServiceName(CmServerServiceType.SOLR)));
+        execute(apiResourceRootV4.getClustersResource().getServicesResource(getName(cluster))
             .createSolrHdfsHomeDirCommand(cluster.getServiceName(CmServerServiceType.SOLR)));
       }
       break;
